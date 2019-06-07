17fb9a853b21d76b60367ac406926f54e68d1b71
akalya23 <akalyaammu26@gmail.com>
2019-06-07 10:15:26 +0530

    Create 22.py

---
+++ 22.py
@@ -0,0 +1,20 @@
+r1=int(input())
+l=list(map(int,input().split()))
+
+f=1
+i=1
+while(i<len(l)+1):
+  f=1
+  j=1
+  for x in l:
+  
+       if(i!=j):
+            
+            f=f*x
+            j=j+1
+       else:
+            j=j+1
+            continue
+  
+  print(f,end=" ")
+  i=i+1
