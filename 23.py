aade0838927f7bfb1d5a9b3a6596542b97baf87e
akalya23 <akalyaammu26@gmail.com>
2019-06-07 10:20:58 +0530

    Create 23.py

---
+++ 23.py
@@ -0,0 +1,32 @@
+n1,m=map(eval,input().split())
+a=[]
+for i in range(n1):
+  a.append(list(map(eval,input().split())))
+pos=[]
+out=[]
+temp_out=[]
+t=[]
+i=0
+j=0
+out.append(a[i][j])
+pos.append([i,j])
+while [n1-1,m-1] not in pos:
+  i=0
+  for x in pos:
+    if x[0]+1<n1 and x[1]+1<m:
+      temp_out.append(a[x[0]+1][x[1]]+out[i])
+      temp_out.append(a[x[0]][x[1]+1]+out[i])
+      t.append([x[0]+1,x[1]])
+      t.append([x[0],x[1]+1])
+    elif x[0]+1<n1:
+      temp_out.append(a[x[0]+1][x[1]]+out[i])
+      t.append([x[0]+1,x[1]])
+    elif x[1]+1<m:
+      temp_out.append(a[x[0]][x[1]+1]+out[i])
+      t.append([x[0],x[1]+1])
+    i+=1
+  pos=t
+  t=[]
+  out=temp_out
+  temp_out=[]
+print(max(out))
