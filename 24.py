afa83b8c79944aef4de608a85e1437b052fe6708
akalya23 <akalyaammu26@gmail.com>
2019-06-07 10:23:06 +0530

    Create 24.py

---
+++ 24.py
@@ -0,0 +1,20 @@
+    
+r1,s=map(int,input().split())
+l=list(map(int,input().split()))
+c=0
+for x in l:
+    if(x>s):
+        l.remove(x)
+for i in range(0,len(l)):
+    j=0
+    for x in l:
+        if(x+l[i]==s and i!=j):
+            
+            c=c+1
+            break
+        j=j+1   
+            
+if (c>0):
+    print("YES")
+else:
+    print("NO")
